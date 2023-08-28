1dc507903d7f9b86bdf68f0709656a84d0fdf306
Gurubarankathirvel <gurubarankathirvel2006@example.com>
2023-08-28 14:21:33 +0530

    databases

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
     numbers=[11,13,14,20]
     return render_template('index.html',numbers=numbers)
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0',port=8080)
